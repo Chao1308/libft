d6db5364351dc4a1b15baa02526d5048a6f708ff
Chaoyi Lin Yang <clin-yan@c2r7s5.42madrid.com>
2024-10-12 13:08:03 +0200

    .h con parte bonus

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: clin-yan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 11:37:45 by clin-yan          #+#    #+#             */
-/*   Updated: 2024/10/08 12:33:16 by clin-yan         ###   ########.fr       */
+/*   Updated: 2024/10/12 13:01:35 by clin-yan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,5 +50,18 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+typedef struct	s_list
+{
+	void	*content;
+	struct s_list	*next;
+}			t_list;
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int	ft_lsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
